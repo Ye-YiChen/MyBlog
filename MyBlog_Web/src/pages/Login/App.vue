a2522459d5108f3d91b348b01469a8b080837cbc
YeYiChen <xuph@hdu.edu.cn>
2022-11-04 22:27:54 +0800

    登录页面完成

--- MyBlog_Web/src/pages/Login/App.vue
+++ MyBlog_Web/src/pages/Login/App.vue
@@ -6,7 +6,37 @@
                 <h4>记录生活的点点滴滴</h4>
             </div>
             <div class="login-box">
-asdas
+                <a-menu mode="horizontal" :default-selected-keys="['1']" class="operate-choice">
+                    <a-menu-item key="1">
+                        <h5>密码登录</h5>
+                    </a-menu-item>
+                </a-menu>
+                <a-form :model="form" :layout="layout">
+                    <a-form-item field="name" required hide-asterisk>
+                        <a-input v-model="form.name" placeholder="please enter your username..." size="large">
+                            <template #prefix>
+                                <icon-user />
+                            </template>
+                        </a-input>
+                    </a-form-item>
+                    <a-form-item field="password" required hide-asterisk>
+                        <a-input-password v-model="form.post" placeholder="please enter your password..." size="large"
+                            allow-clear>
+                            <template #prefix>
+                                <icon-lock />
+                            </template>
+                        </a-input-password>
+                    </a-form-item>
+                    <a-form-item field="isOK">
+                        <a-checkbox v-model="form.isOK">
+                            LET'S ROCK!
+                        </a-checkbox>
+                    </a-form-item>
+                    <a-form-item>
+                        <a-button shape="round" size="large" type="primary" long :disabled="!form.isOK">Submit
+                        </a-button>
+                    </a-form-item>
+                </a-form>
             </div>
         </div>
     </Login>
@@ -15,8 +45,13 @@ asdas
 
 <script setup lang='ts'>
 import Login from '@/layouts/Login.vue';
-
-
+import { ref, reactive } from 'vue';
+const layout = ref<"horizontal" | "vertical" | "inline" | undefined>('vertical')
+const form = reactive({
+    name: '',
+    post: '',
+    isOK: false
+});
 </script>
 
 <style scoped lang='less'>
@@ -48,12 +83,29 @@ h4 {
         letter-spacing: 0.5vw;
         font-weight: 400;
     }
-    .login-box{box-sizing: border-box;
+
+    h5 {
+        margin: 0;
+        font-size: 20px;
+        letter-spacing: .5px;
+        color: black;
+
+    }
+
+    .login-box {
+        box-sizing: border-box;
         margin-top: 5vh;
-        width: 30vw;
+        min-width: 20vw;
+        max-width: 30vw;
         background-color: white;
-        padding: 24px 32px 0;
+        padding: 24px 32px 8px;
         border-radius: 10px;
+        color: black;
+
+        .operate-choice {
+            border-bottom: 1px solid #e8e8e8;
+            margin-bottom: 30px;
+        }
     }
 }
 </style>
