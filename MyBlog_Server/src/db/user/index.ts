503b9830fe69368e792e44203d379385af88dd7e
YeYiChen <xuph@hdu.edu.cn>
2022-11-11 21:46:06 +0800

    1. 实现查询时对deleted_at字段的判断 2. 实现文章和文章分类的软删除

--- MyBlog_Server/src/db/user/index.ts
+++ MyBlog_Server/src/db/user/index.ts
@@ -7,7 +7,7 @@ import { User } from "src/model/User";
 function queryUserLogin(username: string) {
     return new Promise<User[]>((resolve, reject) => {
         username = db.escape(username);
-        const sql = `select user_id, password from user where username=${username}`;
+        const sql = `select user_id, password from user where username=${username} and deleted_at is null `;
         db.query(sql, (err: Error, result: User[]) => {
             if (err) {
                 reject(err);
@@ -27,7 +27,7 @@ function queryUserLogin(username: string) {
 function queryUserInfo(user_id: number) {
     return new Promise<User[]>((resolve, reject) => {
         user_id = parseInt(db.escape(user_id));
-        const sql = `SELECT username, avatar, email, QQ, wechat, github, donation FROM user WHERE user_id=${user_id}`;
+        const sql = `SELECT username, avatar, email, QQ, wechat, github, donation FROM user WHERE user_id=${user_id} and deleted_at is null `;
         db.query(sql, (err: Error, result: User[]) => {
             if (err) {
                 reject(err);
@@ -48,7 +48,7 @@ function updateUserInfo(user_id: number, user: User) {
         user_id = parseInt(db.escape(user_id));
         // 如果有值就更新，没有值就不更新
         const { username, avatar, email, QQ, wechat, github, donation } = user;
-        const sql = `UPDATE user SET username=${username ? db.escape(username) : 'username'}, avatar=${avatar ? db.escape(avatar) : 'avatar'}, email=${email ? db.escape(email) : 'email'}, QQ=${QQ ? db.escape(QQ) : 'QQ'}, wechat=${wechat ? db.escape(wechat) : 'wechat'}, github=${github ? db.escape(github) : 'github'}, donation=${donation ? db.escape(donation) : 'donation'} WHERE user_id=${user_id}`;
+        const sql = `UPDATE user SET username=${username ? db.escape(username) : 'username'}, avatar=${avatar ? db.escape(avatar) : 'avatar'}, email=${email ? db.escape(email) : 'email'}, QQ=${QQ ? db.escape(QQ) : 'QQ'}, wechat=${wechat ? db.escape(wechat) : 'wechat'}, github=${github ? db.escape(github) : 'github'}, donation=${donation ? db.escape(donation) : 'donation'} WHERE user_id=${user_id} and deleted_at is null `;
         db.query(sql, (err: Error, result: any) => {
             if (err) {
                 reject(err);
